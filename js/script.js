55f87c23a1098d0564b654028923b6b4779db07e
babuko24 <babuko2018@gmail.com>
2024-09-21 00:22:12 +0900

    9/20-13

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 var pw;
 pw = prompt('写真を見るにはパスワードが必要だよ。','');
 if(pw != 'ronzephoto-himitudayo'){
-    location.href = pw+'.html';
-}else{
-    alert('パスワードが違うよ');
-}
+    location.href = pw+'.html';}
